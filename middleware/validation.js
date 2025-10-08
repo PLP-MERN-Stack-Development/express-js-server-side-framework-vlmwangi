ba84687a154348e8845b0bbcd0bf7d5053987c48
vlmwangi <lmwangih.lm@gmail.com>
2025-10-08 13:21:34 +0300

    Added Express REST API project files

---
+++ middleware/validation.js
@@ -0,0 +1,9 @@
+const validateProduct = (req, res, next) => {
+  const { name, description, price, category, inStock } = req.body;
+  if (!name || !description || !price || !category || typeof inStock !== "boolean") {
+    return res.status(400).json({ message: "Invalid product data" });
+  }
+  next();
+};
+
+export default validateProduct;
